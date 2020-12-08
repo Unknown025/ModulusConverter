972c8af6ea6d2e1a793d3aca9efb846261197bb5
Unknown025 <11259556+Unknown025@users.noreply.github.com>
2020-12-07 19:08:51 -0800

    Initial commit.

---
+++ src/main/java/org/rainyville/modulusconverter/ModulusConverter.java
@@ -0,0 +1,9 @@
+package org.rainyville.modulusconverter;
+
+import org.fusesource.jansi.AnsiConsole;
+
+public class ModulusConverter {
+    public static void main(String[] args) {
+        AnsiConsole.systemInstall();
+    }
+}
